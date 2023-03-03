278484e329ba2b35876885f6399336ed3222049a
Computastar <jason.hunter@computastar.com>
2023-03-03 14:39:53 +0000

    Added Navbar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+
+import logo from '../../assets/cstar_banner.png'
 import './navbar.css'
 
+const Menu = () => (
+  <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#projects'>Projects</a></p>
+    <p><a href='#contact'>Contact</a></p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className='folio__navbar'>
       <div className='folio__navbar-links'>
-        
+        <div className='folio__navbar-links_logo'>
+          <img src={logo} alt="Logo"/>
+        </div>
+        <div className='folio__navbar-links_container'>
+          <Menu />
+        </div>
+      </div>
+      <div className='folio__navbar-CV'> 
+      <button type="button">Download CV</button>
+      </div>
+      <div className='folio__navbar-menu'>
+        {toggleMenu
+        ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+          <div className="folio-menu_container scale-up-center">
+            <div className="folio__navbar-menu_container-links">
+              <Menu />
+            </div>
+            <div className="folio__navbar-menu_container-links-CV">
+            <button type="button">Download CV</button>
+          </div>
+        </div>
+        )}
       </div>
       NavBar
     </div>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import bio from '../../assets/Bio-Pic.png';
 import './header.css'
 
 const Header = () => {
   return (
-    <div>
+    <div className='folio__header section__Padding' id='home'>
+      <div className='folio__header-content'>
+        <h1 className='gradient__text'> Jason Hunter Portfolio'</h1>
+        <p>The dude is good</p>
+      </div>
+
+      <div className='folio__header-image'>
+        <img src={bio} />
+      </div>
       Header
     </div>
   )
