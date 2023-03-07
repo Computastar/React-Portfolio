dcd3ca832e0e91d3006b7f6ad77d83efe75e29f6
Computastar <jason.hunter@computastar.com>
2023-03-07 20:09:01 +0000

    Got images working

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,35 +1,21 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
 import './project.css'
-import '../../assets/1.jpeg';
-
-
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function Project(props) {
   return (
-    <div>
     <div className='folio__projects-container__project'>
-      <div className='folio__projects-container__project-title'>
-        <div />
-        <h1>{props.name}</h1>
-        </div>
-        <div className='folio__projects-container_project-deployed '>
-          { (props.pages)===null
-            ? <p>No deployed GitHub pages</p>
-            : <a href={props.pages} target="_blank"  rel="noreferrer noopener">View Deployed Site</a>
-          }
-
-      </div>
-      <div className='folio__projects-container_project-repository '>
-        { (props.repo===null)
-          ? <p>No GitHub repository</p>
-          : <a href={props.repo} target="_blank"  rel="noreferrer noopener">View Repository</a>
-        }
+    <Card className='folio__Project-Card' style={{ width: '19rem', height: '19rem' }}>
+    <Card.Img className='folio__Project-Card-image' src={props.image} alt=""/>
+    <Card.Body>
+      <Card.Title className='folio__Project-Card-Title' >{props.name}</Card.Title>
+      <Card.Link className='folio__Project-Card-Link' href={props.pages} target="_blank"  rel="noreferrer noopener">View Deployed Site</Card.Link>
+      <br />
+      <Card.Link className='folio__Project-Card-Link' href={props.repo} target="_blank"  rel="noreferrer noopener">View Repository</Card.Link>
+    </Card.Body>
+  </Card>
     </div>
-    <div className='folio__projects-container_project-image'>
-      <img src={props.image} alt={props.name} />
-    </div>
-  </div>
-  </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from './App';
 import './index.css'
 
+
 import { HashRouter as Router } from "react-router-dom";
 
 
