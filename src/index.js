baf5e11433573d557f6c16f6bab6f1465155100c
Computastar <jason.hunter@computastar.com>
2023-03-07 14:15:44 +0000

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from './App';
 import './index.css'
 
-ReactDOM.render(<App />,document.getElementById('root'));
+import { HashRouter as Router } from "react-router-dom";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Router>
+      <App />
+    </Router>
+);
